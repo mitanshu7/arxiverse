5c36afd44d3f1a25267c9ca5571388bf43de9d5c
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-06-11 15:56:54 +0530

    update main, increase legend,

--- main.py
+++ main.py
@@ -6,38 +6,48 @@ from dash import Dash, html, dcc
 app = Dash()
 server = app.server
 
-parque_file = 'https://media.githubusercontent.com/media/mitanshu7/PaperMatch_Analysis/refs/heads/main/bluuebunny/arxiv_abstract_embedding_mxbai_large_v1_milvus_binary/umap/hamming/all_years.parquet'
-df = pd.read_parquet(parque_file)
+parquet_file = "data/all_years.parquet"
+df = pd.read_parquet(parquet_file)
 
 # Sample a subset of the data for visualization
 df_sampled = df.sample(100000)
 
 # Plot the 3D scatter plot using Plotly Express
-fig = px.scatter_3d(df_sampled, x='x', y='y', z='z', hover_name='title', hover_data='year', color='categories',  opacity=0.5)#, size='year', text='id', symbol='categories')
+fig = px.scatter_3d(df_sampled, x='x', y='y', z='z', hover_name='title', hover_data='year', color='categories',  opacity=0.6)
 
-# Customize the marker size and layout
-fig.update_traces(marker_size = 3)
+# Keep legend markers the same size
 fig.update_layout(
-    title='UMAP Projection of Arxiv Abstracts',
+    legend=dict(
+        itemsizing='constant',  # Use 'constant' to fix marker size in legend
+        title='Categories',
+        font = dict(
+            size=16,  # Set the font size for the legend
+        )
+    )
+)
+
+# Decrease the size of the markers in the plot
+fig.update_traces(marker_size = 1)
+
+# Add a title and axis labels, and adjust the camera angle and height
+fig.update_layout(
+    title='3D Map of Arxiv Abstracts',
     scene=dict(
         xaxis_title='UMAP X',
         yaxis_title='UMAP Y',
         zaxis_title='UMAP Z'
     ),
-    legend_title='Categories',
-    height=800,
+    height=800,  # Set the height of the plot
     scene_camera=dict(
-        eye=dict(x=1.25, y=0, z=0.125)
-    )
+        eye=dict(x=1, y=-0.125, z=0.125)
+    ),
+    scene_dragmode='turntable',  # 'turntable', 'orbit', 'zoom', 'pan', etc
 )
 
 # App layout
 app.layout = [
-    html.Div(children='Interactive 3D Scatter Plot of Arxiv Abstracts.'),
-    html.Div(children='This visualization shows the UMAP projection of Arxiv abstracts, colored by categories.'),
-    html.Div(children='Hover over points to see titles and years. You can zoom, pan, and rotate the plot for better exploration.'),
     dcc.Graph(figure=fig),
-    html.Div(children='Note: The data is sampled for performance reasons.'),
+    # html.Div(children='Note: The data is sampled for performance reasons.'),
 ]
 
 # Run the app
